a60706990b349692ef6ad57ed844f35462187084
Chuck JS. Oh <jinseokoh@hotmail.com>
2021-01-21 09:31:36 +0900

    first commit

---
+++ components/SlotMachine.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="text-center">
+              <!-- <h3 class="display-1 font-weight-black">바른연애생활</h3> -->
+    <div class='d-flex justify-center'>
+      <div class='slot' ref="slot">
+        <div class='slot__window display-1 font-weight-black'>
+          <div class='slot__wrap'>
+            <div class='slot__item' v-for='item in items'>{{ item }}</div>
+            <div class='slot__item slot__item--copy' >{{ items[0] }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h4 class="subtitle-1 font-weight-bold white--text mb-4">친구와 함께 2:2 바른 만남</h4>
+
+    <v-container>
+      <v-row class="d-flex align-center">
+        <v-col cols="12">
+          <v-card width="320" color="transparent" elevation="0" class="mx-auto" style="position: relative;">
+            <v-btn icon @click="start" :loading="!!opt" class="mx-auto">
+              <svg width="64" height="64" viewBox="0 0 480 480" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M374.34 480H105.62C47.29 480 0 432.71 0 374.38V105.62C0 47.29 47.29 0 105.62 0H374.34C432.67 0 479.96 47.29 479.96 105.62V374.39C479.95 432.71 432.67 480 374.34 480Z" fill="#01C5FC"/>
+                <path d="M338.97 213.36C333.8 216.89 326.98 216.89 321.81 213.36C236.02 154.7 275.4 112.14 276.63 110.85C276.65 110.83 276.66 110.82 276.66 110.82C277.03 110.37 277.41 109.94 277.81 109.51C283.58 103.3 291.81 99.43 300.95 99.43C308.46 99.43 315.36 102.05 320.78 106.42C326.49 111.03 334.52 111.03 340.22 106.42C345.64 102.04 352.54 99.43 360.05 99.43C369.19 99.43 377.42 103.31 383.19 109.51C383.58 109.93 383.97 110.37 384.34 110.82C384.34 110.82 384.35 110.83 384.37 110.85C385.6 112.15 424.78 154.7 338.97 213.36Z" fill="#FDCF00"/>
+                <path d="M158.65 213.36C153.48 216.89 146.66 216.89 141.49 213.36C55.7 154.7 95.08 112.15 96.31 110.85C96.33 110.83 96.34 110.82 96.34 110.82C96.71 110.37 97.09 109.94 97.49 109.51C103.26 103.3 111.49 99.43 120.63 99.43C128.14 99.43 135.04 102.05 140.46 106.42C146.17 111.03 154.2 111.03 159.9 106.42C165.32 102.04 172.22 99.43 179.73 99.43C188.87 99.43 197.1 103.31 202.87 109.51C203.26 109.93 203.65 110.37 204.02 110.82C204.02 110.82 204.03 110.83 204.05 110.85C205.27 112.15 244.45 154.7 158.65 213.36Z" fill="#FDCF00"/>
+                <path d="M331.25 240.27C318.27 240.27 307.76 250.79 307.76 263.77V280.15H267.24C254.32 280.15 243.85 290.62 243.85 303.54C243.85 316.46 254.32 326.93 267.24 326.93H307.76V345.06H267.24C254.32 345.06 243.85 355.53 243.85 368.45C243.85 381.37 254.32 391.84 267.24 391.84H307.76V391.87H331.08C344.03 391.87 354.54 381.37 354.54 368.41V263.55C354.53 250.69 344.11 240.27 331.25 240.27Z" fill="white"/>
+                <path d="M213.12 326.92C226.04 326.92 236.51 316.45 236.51 303.53C236.51 290.61 226.04 280.14 213.12 280.14H172.6V263.76C172.6 250.78 162.08 240.26 149.11 240.26C136.25 240.26 125.83 250.68 125.83 263.54V368.4C125.83 381.36 136.33 391.86 149.29 391.86H172.61V391.83H213.13C226.05 391.83 236.52 381.36 236.52 368.44C236.52 355.52 226.05 345.05 213.13 345.05H172.61V326.92H213.12Z" fill="white"/>
+              </svg>
+            </v-btn>
+            <img src="/images/click.png" style="position: absolute; right: 0" />
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card elevation="0" color="transparent" tile>
+            <a href="https://www.apple.com/kr/app-store/">
+              <v-img :class="leftAlign" src="/images/apple.png" width="190" height="56" />
+            </a>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card elevation="0" color="transparent" tile>
+            <a href="https://play.google.com/store">
+              <v-img :class="rightAlign" src="/images/google.png" width="220" height="64" />
+            </a>
+          </v-card>
+        </v-col>
+      </v-row>
+
+
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+const next = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  function(cb) { window.setTimeout(cb, 1000/60) }
+
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      items: [
+        '바른연애생활',
+        '바른문화생활',
+        '바른운동생활',
+        '바른음주생활',
+        '바른취미생활',
+        '바른게임생활',
+        '바른요리생활',
+      ],
+      opt: null,
+      startedAt: null,
+    }
+  },
+  computed: {
+    rightAlign() {
+      return this.$vuetify.breakpoint.mdAndUp ? "mr-auto" : "mx-auto"
+    },
+    leftAlign() {
+      return this.$vuetify.breakpoint.mdAndUp ? "ml-auto" : "mx-auto"
+    },
+  },
+  methods: {
+    start: function() {
+      if (this.opt) {
+        return
+      }
+      const slot = this.$refs.slot
+      const choice = Math.floor( Math.random() * this.items.length )
+
+      this.opt = {
+        el: slot.querySelector('.slot__wrap'),
+        finalPos: choice * 64,
+        startOffset: 2000 + Math.random() * 500,
+        height: this.items.length * 64,
+        duration: 3000, // milliseconds
+        isFinished: false,
+      }
+      
+      next(this.animate)
+    },
+    
+    animate: function(timestamp) {
+			if (this.startedAt == null) {
+        this.startedAt = timestamp
+      }
+      const timeDiff = timestamp - this.startedAt
+      
+      if (this.opt.isFinished) {
+        return
+      }
+
+      const timeRemaining = Math.max(this.opt.duration - timeDiff, 0)
+      const power = 3
+      const offset = ( Math.pow(timeRemaining, power) / Math.pow(this.opt.duration, power) ) * this.opt.startOffset
+        
+      // negative, such that slots move from top to bottom
+      const pos = -1 * Math.floor((offset + this.opt.finalPos) % this.opt.height)
+      this.opt.el.style.transform = "translateY(" + pos + "px)"
+        
+      if ( timeDiff > this.opt.duration ) {
+        console.log('finished', this.opt, pos, this.opt.finalPost)
+        this.opt.isFinished = true
+        this.opt = null
+        this.startedAt = null
+      } else {
+        next( this.animate )
+      }
+    },
+  }
+
+}
+</script>
+
+<style>
+
+.slot__window {
+  background-color: green;
+  width: 240px;
+  height: 64px;
+  overflow: hidden;
+}
+
+.slot__wrap {
+
+}
+
+.slot__item {
+  width: 240px;
+  height: 64px;
+
+  text-align: center;
+  background-color: #53c6ff;
+  color: black;
+  
+  line-height: 64px;
+}
+
+.slot__item--copy {
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,303 @@
+<template>
+<div>
+  <v-container>
+    <v-card elevation="0">
+      <v-row>
+        <v-col cols="6">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    tile
+                  >
+                    <v-img src="/images/mascot1.png" data-aos="fade-right" />
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+        <v-col cols="6" class="d-flex justify-start align-center">
+          <section>
+            <v-card
+              class="mx-auto"
+              elevation="0"
+              tile
+            >
+              <v-card-title :class="largeFont" class="d-flex flex-column font-weight-bold">
+                <div v-if="this.$vuetify.breakpoint.mdAndUp">
+                  <div class="black--text mb-2">
+                  바른연애생활은
+                  </div>
+                  <div class="light-blue--text">
+                  낯선사람의 얼굴만보고 즉석 만남을 갖을 수는 없습니다...
+                  </div>
+                </div>
+                <div v-else>
+                  <span class="black--text mb-2">
+                  바른연애생활은
+                  </span>
+                  <span class="light-blue--text">
+                  낯선사람의 얼굴만보고 즉석 만남을 갖을 수는 없습니다...
+                  </span>
+                </div>
+              </v-card-title>
+            </v-card>
+          </section>
+        </v-col>
+      </v-row>
+      <v-row :style="padding">
+        <v-col cols="6"  class="d-flex justify-end align-center">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    tile
+                  >
+                    <v-card-title :class="largeFont" class="d-flex flex-column font-weight-bold">
+                      <div v-if="this.$vuetify.breakpoint.mdAndUp">
+                        <div class="black--text mb-2">
+                        바른연애생활은
+                        </div>
+                        <div class="light-blue--text">
+                        지인과 함께 모임을 개설하고 참가자들을 모집합니다.
+                        </div>
+                      </div>
+                      <div v-else>
+                        <span class="black--text mb-2">
+                        바른연애생활은
+                        </span>
+                        <span class="light-blue--text">
+                        지인과 함께 모임을 개설하고 참가자들을 모집합니다.
+                        </span>
+                      </div>
+                    </v-card-title>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+        <v-col cols="6">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    tile
+                  >
+                    <v-img src="/images/mascot2.png" data-aos="fade-left" />
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+
+  <v-card elevation="0" style="background: #fafafa;">
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    color="transparent"
+                    tile
+                  >
+                    <v-img src="/images/screenshot1.png" data-aos="flip-right" />
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+        <v-col cols="6" class="d-flex justify-end align-center">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    tile
+                    color="transparent"
+                  >
+                    <v-card-title :class="largeFont" class="font-weight-black">
+                      지인과 함께 2:2 모임
+                    </v-card-title>
+                    <v-card-text v-if="this.$vuetify.breakpoint.mdAndUp" :class="mediumFont" class="font-weight-bold grey--text">
+                      지인에게 숨겨가면서 낯선 사람과 1:1 로 만나는 것이 부담스러웠다면 지인과 함께 2:2 로 만나는 모임에 참가해보세요.
+                    </v-card-text>
+                    <v-card-text v-else :class="mediumFont" class="font-weight-bold grey--text">
+                      실제 친구를 바연생에 초대하여 안전하게 모임을 즐기세요.
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+  <v-card elevation="0">
+    <v-container>
+      <v-row>
+        <v-col cols="6" class="d-flex justify-end align-center">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    color="transparent"
+                    tile
+                  >
+                    <v-card-title :class="largeFont" class="font-weight-black">
+                      외모·스펙지상주의?!
+                    </v-card-title>
+                    <v-card-text v-if="this.$vuetify.breakpoint.mdAndUp" :class="mediumFont" class="font-weight-bold grey--text">
+                      나를 평가의 대상으로 판단하는 사람들보다 나의 생각과 관심에 공감하는 사람들과 만나보세요.
+                    </v-card-text>
+                    <v-card-text v-else :class="mediumFont" class="font-weight-bold grey--text">
+                      외모보다 나와 공감할 수 있는 사람들과 만나보세요.
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+        <v-col cols="6">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    color="transparent"
+                    tile
+                  >
+                    <v-img src="/images/screenshot2.png" data-aos="flip-left" />
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+  <v-card elevation="0" style="background: #fafafa;">
+    <v-container>
+      <v-row>
+        <v-col cols="6">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    color="transparent"
+                    tile
+                  >
+                    <v-img src="/images/screenshot3.png" data-aos="flip-right" />
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+        <v-col cols="6"  class="d-flex justify-end align-center">
+          <section>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card
+                    elevation="0"
+                    color="transparent"
+                    tile
+                  >
+                    <v-card-title :class="largeFont" class="font-weight-black">
+                      취미를 공유하는 의미있는 만남
+                    </v-card-title>
+                    <v-card-text v-if="this.$vuetify.breakpoint.mdAndUp" :class="mediumFont" class="font-weight-bold grey--text">
+                      지역별, 관심사별, 또래별 모임들을 검색하고 지인과 함께 2:2 모임에 동참해 보세요.
+                    </v-card-text>
+                    <v-card-text v-else :class="mediumFont" class="font-weight-bold grey--text">
+                      만나서 같이 즐기고 싶은 모임 유형을 선택하세요.
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </section>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+
+  <div :style="padding"></div>
+
+  <marquee-text class="font-weight-bold grey--text" :duration="30">
+    <v-chip class="mx-2" color="#53c6ff" text-color="white" label>바연생 10계명</v-chip>
+    <span v-for="(label, i) in labels" :key="i">
+      <span>{{ label }}</span>
+    </span>
+  </marquee-text>
+  
+  <div :style="padding"></div>
+
+</div>
+</template>
+
+<script>
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import MarqueeText from 'vue-marquee-text-component'
+
+AOS.init({ once: true })
+
+export default {
+  components: {
+    MarqueeText,
+  },
+  data() {
+    return {
+      labels: [
+        '✋ 모태솔로라고 남의 애인을 탐하지 마라. ',
+        '✋ 없다고 서러워말고 바연생으로 될 때까지 시도하라. ',
+        '✋ 상대방이 내 스탈일수록 친구에게 무조건적 양보를 강요하지 마라. ',
+        '✋ 프로파일 사진은 반드시 마스크 벗은 사진으로 올리시라. ',
+        '✋ 업무시간에 숨어서 채팅하다 실실 쪼개지 마라. ',
+        '✋ 채팅방에서 절대 음란대화 하지 마라. ',
+        '✋ 생고기는 꼭 익혀드시라. ',
+        '✋ 에스컬레이터에서 뛰거나 걷지 마라. ',
+        '✋ 5년 전에 비트코인 샀다고 자랑하지 마라. ',
+        '✋ 다시보니 박명수랑 닯았다고 말하지 마라. ',
+      ]
+    }
+  },
+  computed: {
+    padding() {
+      return this.$vuetify.breakpoint.mdAndUp ? { 'padding': '48px 0' } : { 'padding': '12px 0' }
+    },
+    largeFont() {
+      return this.$vuetify.breakpoint.mdAndUp ? 'display-2' : 'h6'
+    },
+    mediumFont() {
+      return this.$vuetify.breakpoint.mdAndUp ? 'display-1' : 'body-2'
+    },
+  },
+}
+</script>
